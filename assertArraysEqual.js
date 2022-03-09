c98955f01b53ea7824e0dbd580fb7f12da47f382
Rick <rickywu209@gmail.com>
2022-03-09 05:02:16 +0000

    checks

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,25 @@
 const assertArraysEqual = function(arr1, arr2){
-  if(eqArrays()) {
+  if(eqArrays(arr1, arr2) === true) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
-}
+};
 
 
 const eqArrays = function(arr1, arr2){
-  for(let i = 0; i < arr1.length; i++){
-    if(arr1[i] === arr2[i] || arr1.length === arr2.length) {
-      return true;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]) {
+      return false
     }
   }
-  return false;
-};
+  return true
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [3, 2, 1])
+assertArraysEqual(['1', '2', '3'], ['1', '2', '3'])
+assertArraysEqual(['1', '2', '3'], ['1', '2', 3])
---
+++ without.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = function(arr1, arr2){
+  if(eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+const without = function(source, itemsToRemove){
+ let newArray = [];
+
+  source.forEach(element => {
+   if (!itemsToRemove.includes(element)){
+     newArray.push(element)
+   }
+ })
+ return newArray;
+  }
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
