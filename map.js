0ba1547a10b15e5cfad4c39a67ba6771047a6617
Rick <rickywu209@gmail.com>
2022-03-10 23:28:38 +0000

    map

---
+++ map.js
@@ -0,0 +1,34 @@
+const words = ["pepsi", "coke", "sprite", "fanta", "tea"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const assertArraysEqual = function(arr1, arr2){
+  if(eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1); 
+
