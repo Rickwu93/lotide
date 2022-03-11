ca0946055e6e23650f67f3755c48bdbf892f3790
Rick <rickywu209@gmail.com>
2022-03-11 03:43:31 +0000

    key

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}; 
+
+//function takes in an object and a callback 
+//return first key which callback returns a truthy value
+//else return undefined
+
+const findKey = function(object, callback) {
+  for(const item in object) {
+    if(callback(object[item])) {
+      return item;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');
